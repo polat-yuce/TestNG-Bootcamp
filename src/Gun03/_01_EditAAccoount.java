eb626be7ab71393b3c39ceba24120a20b958adb0
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-05-09 14:09:12 +0300

    gun03

---
+++ src/Gun03/_01_EditAAccoount.java
@@ -0,0 +1,37 @@
+package Gun03;
+
+import Utlity.BaseDriver;
+import Utlity.Tools;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+/*
+  Senaryo
+  1- Siteyi açınız.
+  2- Edit Account sayfasına ulaşınız.
+  3- Ad ve soyadı değiştirip tekrar kaydedidiniz.
+ */
+
+public class _01_EditAAccoount extends BaseDriver {
+
+    @Test
+    public void EditAccount(){
+        WebElement editLink=driver.findElement(By.linkText("Edit Account"));
+        editLink.click();
+
+        WebElement txtFirstName=driver.findElement(By.id("input-firstname"));
+        txtFirstName.clear();
+        txtFirstName.sendKeys("ismet 2");
+
+        WebElement txtLastName=driver.findElement(By.id("input-lastname"));
+        txtLastName.clear();
+        txtLastName.sendKeys("temur 2");
+
+        WebElement btnSubmit=driver.findElement(By.xpath("//input[@type='submit']"));
+        btnSubmit.click();
+
+        Tools.SuccessMessageValidation();
+    }
+
+}
