e9308bc97ab4a6a0ace24c9df7ccd232172ec070
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-05-21 22:10:01 +0300

    gun06

---
+++ src/Gun06/_02_Soru.java
@@ -0,0 +1,11 @@
+package Gun06;
+
+/*
+  TODO:
+  Senaryo;
+  1- https://www.ebay.com/n/all-brands  menusune gidiniz.
+  2- Burada her bir markanın üstünde yer alan harf ile başladığını doğrulayınız
+*/
+
+public class _02_Soru {
+}
